06cb75076590b3a65881321142d140f25b9228f3
Erick Alcantar <erickalcantar@hotmail.com>
2026-05-16 18:33:29 -0500

    Cambiando el programa para mejorar las salidas de resultado

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@ int main() {
     const string name = "Erick";
     int list_ages[] = { 10, 20, 30};
     list_ages[2] = age;
-    cout << list_ages[1] << endl;
+    cout << "Ages list size: " << sizeof(list_ages)/sizeof(list_ages[0]) << endl;
     cout << name + ". " + "Age: " + to_string(age)<< endl;
-    cout << sizeof(list_ages)/sizeof(list_ages[0]) << endl;
 
     //Basics of IO Stream
     cout << "Change age? ";
