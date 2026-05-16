4a0f2a189fd090116e0bdc2b157836d37bfc1f8c
Erick Alcantar <erickalcantar@hotmail.com>
2026-05-16 18:29:27 -0500

    IO Stream basics

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    int age = 10;
+    //Basics of Lists
+    int age = 19;
     const string name = "Erick";
     int list_ages[] = { 10, 20, 30};
     list_ages[2] = age;
     cout << list_ages[1] << endl;
-    cout << name << endl;
+    cout << name + ". " + "Age: " + to_string(age)<< endl;
+    cout << sizeof(list_ages)/sizeof(list_ages[0]) << endl;
+
+    //Basics of IO Stream
+    cout << "Change age? ";
+    cin >> age;
+    cout << name + ". " + "Age: " << age << endl;
     return 0;
 }
